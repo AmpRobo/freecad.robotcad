e90483c73bb67bb3ff96ec079d6de32c1a587d03
Xiaoyu0601-Wang <micheal.wang@hotmail.com>
2026-05-10 15:11:06 +0900

    fix: change installation path

--- install-modules.sh
+++ install-modules.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REPOS_FILE="${ROOT}/module.repos"
 MODULES_DIR="${ROOT}/modules"
 
